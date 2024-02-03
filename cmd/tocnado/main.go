0a1e0b4f86aa9c3cf6dae141d61157326ec46ad3
Jørgen Gundersen <jg@gundersenj.com>
2024-02-03 06:20:14 +0100

    feat: capture headlines and transforms to anchor links

--- cmd/tocnado/main.go
+++ cmd/tocnado/main.go
@@ -9,17 +9,30 @@ import (
 )
 
 type Headline struct {
+	rawText     string
 	text        string
 	level       int
 	chainedText string
+	headlineID  string
+	anchorLink  string
 }
 
-func newHeadline(text string, level int) Headline {
+func newHeadline(rawHeadline string) Headline {
+	headlinePattern := `(#+)\s(.*)`
+	headlineRegex := regexp.MustCompile(headlinePattern)
+
+	headlineParts := headlineRegex.FindStringSubmatch(rawHeadline)
+
 	headline := Headline{
-		text:        text,
-		level:       level,
-		chainedText: strings.ToLower(strings.Replace(text, " ", "-", -1)),
+		rawText: rawHeadline,
+		text:    string(headlineParts[2]),
+		level:   len(headlineParts[1]),
 	}
+
+	headline.chainedText = strings.ToLower(strings.Replace(headline.text, " ", "-", -1))
+	headline.headlineID = "#" + headline.chainedText
+	headline.anchorLink = fmt.Sprintf("[%s](%s)", headline.text, headline.headlineID)
+
 	return headline
 }
 
@@ -27,30 +40,51 @@ type TableOfContent struct {
 	headlines []Headline
 }
 
-func main() {
-	file, err := os.Open("test.md")
+func getFileLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println(err)
+		return nil, err
 	}
 	defer file.Close()
 
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func getHeadlines(lines []string) []Headline {
 	headlinePattern := `(#+)\s(.*)`
 	headlineRegex := regexp.MustCompile(headlinePattern)
 
-	scanner := bufio.NewScanner(file)
 	var headlines []Headline
-	for scanner.Scan() {
-		line := scanner.Text()
+	for _, line := range lines {
 		if headlineRegex.MatchString(line) {
-			match := headlineRegex.FindStringSubmatch(line)
-			headline := newHeadline(match[2], len(match[1]))
+			headline := newHeadline(line)
 			headlines = append(headlines, headline)
 		}
 	}
+	return headlines
+}
 
-	fmt.Println(headlines)
-
-	if err := scanner.Err(); err != nil {
+func main() {
+	filePath := "test.md"
+	lines, err := getFileLines(filePath)
+	if err != nil {
 		fmt.Println(err)
+	}
+
+	headlines := getHeadlines(lines)
+
+	for _, headline := range headlines {
+		fmt.Println(headline.anchorLink)
 	}
 }
